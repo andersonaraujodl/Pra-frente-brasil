5733bbe92c0e6f2171fdef72448084be11225334
Marcelo Pietragalla <mpietragalla@gmail.com>
2014-10-28 12:03:34 -0200

    Atualização graphInitObjects e inclusão da função updateScreen

--- source/graph/grafico.h
+++ source/graph/grafico.h
@@ -1,17 +1,25 @@
+/**
+ *  @file grafico.h
+ *  @brief Arquivo contendo as definições gráficas utilizadas no projeto
+ */
+
 #ifndef _FORMULAS_H__
 #define _FORMULAS_H__
 
 #include "../h/types.h"
 
-#define SCREEN_W 1024
-#define SCREEN_H 600
-#define SCREEN_CENTER SCREEN_W/2
-#define PLAYER_SPOT SCREEN_W/3
-#define BGCOLOR RED
+/**
+	 *  @brief Definições globais da tela
+	 */
+
+#define SCREEN_W 1024 /**< Largura da tela*/
+#define SCREEN_H 600 /**< Altura da tela*/
+#define SCREEN_CENTER SCREEN_W/2 /**< centro horizontal da tela*/
+#define PLAYER_SPOT SCREEN_W/3 /**< Posição horizontal da tela que será o ponto fixo do personagem*/
+#define BGCOLOR BGCOLOR COLOR(100,50,50) /**< cor de fundo da tela*/
 
 
 /*typedef struct{
-	int type;
 	void *img;
 	vetor2d_type pos;
 	float h;
@@ -21,10 +29,10 @@
 
 
 // Protótipo das funções ------------------------------
-void print(graph_data_type *player);
-void erase(graph_data_type *player);
-void clear();
-void initObjects(graph_data_type *player);
+void updateScreen();
+void print(graph_data_type *objeto);
+void erase();
+void graphInitObjects(graph_data_type *objeto, const char* caminho);
 void printBG(graph_data_type *player, graph_data_type *background, vetor2d_type screenmax);
 
 #endif
